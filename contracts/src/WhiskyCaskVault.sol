3d197f716cc19c13fc2444620e953cca2301e761
Tim Johnson <johnson9633@gmail.com>
2026-02-06 12:37:36 -0800

    Remove UTF-8 BOM markers from Solidity source files

--- contracts/src/WhiskyCaskVault.sol
+++ contracts/src/WhiskyCaskVault.sol
@@ -1,4 +1,4 @@
-﻿// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
 import {IWhiskyCaskVault} from "./interfaces/IWhiskyCaskVault.sol";
--- contracts/src/interfaces/IWhiskyCaskVault.sol
+++ contracts/src/interfaces/IWhiskyCaskVault.sol
@@ -1,4 +1,4 @@
-﻿// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
 interface IWhiskyCaskVault {
